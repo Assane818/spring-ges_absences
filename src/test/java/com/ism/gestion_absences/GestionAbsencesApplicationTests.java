715984012bcae6dd32dc2fb944522f5878c5be37
Assane818 <assanen818@gmail.com>
2025-05-26 20:47:05 +0000

    first commit

---
+++ src/test/java/com/ism/gestion_absences/GestionAbsencesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ism.gestion_absences;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAbsencesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
