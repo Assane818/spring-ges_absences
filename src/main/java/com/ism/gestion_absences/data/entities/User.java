9de24ca3cb724f1944091f5e2063196c5c80888a
Assane818 <assanen818@gmail.com>
2025-05-28 19:01:39 +0000

    Creation des repo, service, controller et ajout de la documentation swager

--- src/main/java/com/ism/gestion_absences/data/entities/User.java
+++ src/main/java/com/ism/gestion_absences/data/entities/User.java
@@ -1,12 +1,10 @@
 package com.ism.gestion_absences.data.entities;
 
-import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@MappedSuperclass
 public class User extends AbstractEntity {
     private String nom;
     private String prenom;
